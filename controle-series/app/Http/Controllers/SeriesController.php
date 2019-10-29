c5850f8537ab46cf94a0a078bbc574f01b53ec76
Lucas Eduardo Beninca <lebbeninca@gmail.com>
2019-10-29 16:29:54 -0300

    configurando ambiente
    projeto em php com laravel, utilizando composer, para organizar series assitidas.

---
+++ controle-series/app/Http/Controllers/SeriesController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class SeriesController extends Controller {
+
+public  function listarSeries () {
+      $series = [
+          'Supernatural',
+          'The ranch',
+          'pesadelo na cozinha'
+      ];
+
+      $html = "<ul>";
+      foreach ($series as $serie) {
+          $html .= "<li>$serie</li>";
+      }
+      $html .= "</ul>";
+
+      return $html;
+    }
+}
+
+
+ ?>
