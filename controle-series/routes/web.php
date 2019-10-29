3095c22a41b2a90d8de29445da28aba96a46f04c
Lucas Eduardo Beninca <lebbeninca@gmail.com>
2019-10-29 19:18:34 -0300

    update

--- controle-series/routes/web.php
+++ controle-series/routes/web.php
@@ -1,11 +1,5 @@
 <?php
 
-//use Symfony\Component\Routing\Route;
-
-Route::get('/', function () {
-    return view('welcome');
-});             
-
 // series: esse é o caminho que do navegador
 // SeriesController: é a classe que ele deve chamar quando eu executar o /series no navegador
 // @listarSeries é a função dentro da class SeriesController que ele deve executar
