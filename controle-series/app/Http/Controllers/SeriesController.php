ce7266905c7184e18f48f03c6f5964aaae3b9ca5
Lucas Eduardo Beninca <lebbeninca@gmail.com>
2019-12-15 20:09:02 -0300

    Retomando aos estudos

--- controle-series/app/Http/Controllers/SeriesController.php
+++ controle-series/app/Http/Controllers/SeriesController.php
@@ -1,25 +1,26 @@
 <?php
-
 namespace App\Http\Controllers;
 
-class SeriesController extends Controller {
 
-public  function listarSeries () {
-      $series = [
-          'Supernatural',
-          'The ranch',
-          'pesadelo na cozinha'
-      ];
-
-      $html = "<ul>";
-      foreach ($series as $serie) {
-          $html .= "<li>$serie</li>";
-      }
-      $html .= "</ul>";
-
-      return $html;
+class SeriesController extends Controller
+{
+    public function listarSeries () {
+        $series = [
+            'Grey\'s anatomy', 'supernatural', 'nos somos a onda'
+        ];
+    
+        $html = "<ul>";
+        foreach ($series as $serie) {
+            $html .= "<li>$serie</li>";
+        }
+        $html .= "</ul>";
+    
+        return $html;
     }
 }
 
 
- ?>
+
+
+
+?>
--- controle-series/routes/web.php
+++ controle-series/routes/web.php
@@ -1,8 +1,20 @@
 <?php
 
-// series: esse é o caminho que do navegador
-// SeriesController: é a classe que ele deve chamar quando eu executar o /series no navegador
-// @listarSeries é a função dentro da class SeriesController que ele deve executar
-Route::get ('/series', 'SeriesController@listarSeries');
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
 
-?>
+use Illuminate\Routing\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/series', 'SeriesController@listarSeries');
